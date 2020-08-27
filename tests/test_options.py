c3e19c811975578bad94bd413a453d26a82dd29b
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-08-28 00:43:32 +0200

    plain-vanilla options tests added

--- tests/test_options.py
+++ tests/test_options.py
@@ -26,8 +26,14 @@ class TestPlainVanillaOption(unittest.TestCase):
         S_vector = [90, 100, 110]
         t_scalar_string = "01-06-2020"
         t_date_range = pd.date_range(start="2020-04-19", end="2020-12-21", periods=5)
-        self.vector_params = {"S": S_vector, "t": t_date_range, "np_output": True}
-        self.scalar_params = {"S": S_scalar, "t": t_scalar_string, "np_output": True}
+        self.scalar_params = {"S": S_scalar, "t": t_scalar_string}
+        self.vector_params = {"S": S_vector, "t": t_date_range}
+
+        # other setup parameters
+        self.scalar_params["np_output"] = True
+        self.vector_params["np_output"] = True
+        self.scalar_params["minimization_method"] = "Least-Squares"
+        self.vector_params["minimization_method"] = "Least-Squares"
 
     def test_price_scalar(self):
         """Test price - scalar case"""
@@ -89,6 +95,73 @@ class TestPlainVanillaOption(unittest.TestCase):
                                           [9.75553655e+00, 3.92142545e+00, 1.16826738e+00],
                                           [9.62127704e+00, 3.03816479e+00, 5.26025639e-01],
                                           [9.86382907e+00, 1.25238707e+00, 1.75090342e-03]],
+                                    index=self.vector_params["t"],
+                                    columns=self.vector_params["S"])
+
+        expected_put.rename_axis("S", axis='columns', inplace=True)
+        expected_put.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_put, expected_put)
+
+    def test_PnL_scalar(self):
+        """Test P&L - scalar case"""
+
+        # call
+        test_call = scalarize(self.call_opt.PnL(**self.scalar_params))
+        expected_call = 4.060979245868182
+        self.assertEqual(test_call, expected_call)
+
+        # put
+        test_put = scalarize(self.put_opt.PnL(**self.scalar_params))
+        expected_put = -5.368600081057167
+        self.assertEqual(test_put, expected_put)
+
+    def test_PnL_vector_np(self):
+        """Test P&L - np.ndarray output case"""
+
+        # call
+        test_call = self.call_opt.PnL(**self.vector_params)
+        expected_call = np.array([[0.,           4.93782966, 12.10940574],
+                                  [-0.95695119,  3.6542734,  10.83437716],
+                                  [-1.92644469,  2.2394442,   9.48628613],
+                                  [-2.89823717,  0.51865057,  8.00651142],
+                                  [-3.48668089, -2.09812288,  6.65124095]])
+        np_test.assert_allclose(test_call, expected_call)
+
+        # put
+        test_put = self.put_opt.PnL(**self.vector_params)
+        expected_put = np.array([[0.,           -5.06217034,  -7.89059426],
+                                 [-0.13341186,  -5.52218727,  -8.3420835 ],
+                                 [-0.28579403,  -6.11990513,  -8.87306321],
+                                 [-0.42005355,  -7.0031658,   -9.51530495],
+                                 [-0.17750152,  -8.78894351, -10.03957968]])
+        np_test.assert_allclose(test_put, expected_put)
+
+    def test_PnL_vector_df(self):
+        """Test P&L - pd.DataFrame output case"""
+
+        # request Pandas DataFrame as output format
+        self.vector_params["np_output"] = False
+
+        # call
+        test_call = self.call_opt.PnL(**self.vector_params)
+        expected_call = pd.DataFrame(data=[[0.,           4.93782966, 12.10940574],
+                                           [-0.95695119,  3.6542734,  10.83437716],
+                                           [-1.92644469,  2.2394442,   9.48628613],
+                                           [-2.89823717,  0.51865057,  8.00651142],
+                                           [-3.48668089, -2.09812288,  6.65124095]],
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_call.rename_axis("S", axis='columns', inplace=True)
+        expected_call.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_call, expected_call)
+
+        # put
+        test_put = self.put_opt.PnL(**self.vector_params)
+        expected_put = pd.DataFrame(data=[[0.,           -5.06217034,  -7.89059426],
+                                          [-0.13341186,  -5.52218727,  -8.3420835],
+                                          [-0.28579403,  -6.11990513,  -8.87306321],
+                                          [-0.42005355,  -7.0031658,   -9.51530495],
+                                          [-0.17750152,  -8.78894351,  -10.03957968]],
                                     index=self.vector_params["t"],
                                     columns=self.vector_params["S"])
 
@@ -227,7 +300,7 @@ class TestPlainVanillaOption(unittest.TestCase):
         pd_test.assert_frame_equal(test_call, test_put)
 
     def test_vega_scalar(self):
-        """Test vega - scalar case"""
+        """Test Vega - scalar case"""
 
         # call
         test_call = scalarize(self.call_opt.vega(**self.scalar_params))
@@ -288,6 +361,188 @@ class TestPlainVanillaOption(unittest.TestCase):
 
         # assert call and put vega coincide
         pd_test.assert_frame_equal(test_call, test_put)
+
+    def test_theta_scalar(self):
+        """Test Theta - scalar case"""
+
+        # call
+        test_call = scalarize(self.call_opt.theta(**self.scalar_params))
+        expected_call = -0.021064685979455443
+        self.assertEqual(test_call, expected_call)
+
+        # put
+        test_put = scalarize(self.put_opt.theta(**self.scalar_params))
+        expected_put = -0.007759980665812141
+        self.assertEqual(test_put, expected_put)
+
+    def test_theta_vector_np(self):
+        """Test Theta - np.ndarray output case"""
+
+        # call
+        test_call = self.call_opt.theta(**self.vector_params)
+        expected_call = np.array([[-0.01516655, -0.01977662, -0.01990399],
+                                  [-0.01569631, -0.02176239, -0.0212802],
+                                  [-0.01601397, -0.02491789, -0.02297484],
+                                  [-0.01474417, -0.03162919, -0.02457737],
+                                  [-0.00046144, -0.0728981, -0.01462746]])
+        np_test.assert_allclose(test_call, expected_call, rtol=5e-4)
+
+        # put
+        test_put = self.put_opt.theta(**self.vector_params)
+        expected_put = np.array([[-0.00193999, -0.00655005, -0.00667743],
+                                 [-0.00235693, -0.00842301, -0.00794082],
+                                 [-0.00256266, -0.01146658, -0.00952353],
+                                 [-0.00117813, -0.01806315, -0.01101133],
+                                 [0.01321844, -0.05921823, -0.00094758]])
+        np_test.assert_allclose(test_put, expected_put, rtol=1e-5)
+
+    def test_theta_vector_df(self):
+        """Test Theta - pd.DataFrame output case"""
+
+        # request Pandas DataFrame as output format
+        self.vector_params["np_output"] = False
+
+        # call
+        test_call = self.call_opt.theta(**self.vector_params)
+        expected_call = pd.DataFrame(data=[[-0.01516655, -0.01977662, -0.01990399],
+                                           [-0.01569631, -0.02176239, -0.0212802],
+                                           [-0.01601397, -0.02491789, -0.02297484],
+                                           [-0.01474417, -0.03162919, -0.02457737],
+                                           [-0.00046144, -0.0728981, -0.01462746]],
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_call.rename_axis("S", axis='columns', inplace=True)
+        expected_call.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_call, expected_call, check_less_precise=True)
+
+        # put
+        test_put = self.put_opt.theta(**self.vector_params)
+        expected_put = pd.DataFrame(data=[[-0.00193999, -0.00655005, -0.00667743],
+                                          [-0.00235693, -0.00842301, -0.00794082],
+                                          [-0.00256266, -0.01146658, -0.00952353],
+                                          [-0.00117813, -0.01806315, -0.01101133],
+                                          [0.01321844, -0.05921823, -0.00094758]],
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_put.rename_axis("S", axis='columns', inplace=True)
+        expected_put.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_put, expected_put, check_less_precise=True)
+
+    def test_rho_scalar(self):
+        """Test Rho - scalar case"""
+
+        # call
+        test_call = scalarize(self.call_opt.rho(**self.scalar_params))
+        expected_call = 0.309243166487844
+        self.assertEqual(test_call, expected_call)
+
+        # put
+        test_put = scalarize(self.put_opt.rho(**self.scalar_params))
+        expected_put = -0.2575372798733608
+        self.assertEqual(test_put, expected_put)
+
+    def test_rho_vector_np(self):
+        """Test Rho - np.ndarray output case"""
+
+        # call
+        test_call = self.call_opt.rho(**self.vector_params)
+        expected_call = np.array([[2.08128741e-01, 3.72449469e-01, 5.12209444e-01],
+                                  [1.39670999e-01, 2.81318986e-01, 4.02292404e-01],
+                                  [7.76651463e-02, 1.91809707e-01, 2.90026614e-01],
+                                  [2.49657984e-02, 1.01399432e-01, 1.68411513e-01],
+                                  [2.17415573e-05, 1.39508485e-02, 2.73093423e-02]])
+        np_test.assert_allclose(test_call, expected_call, rtol=1e-5)
+
+        # put
+        test_put = self.put_opt.rho(**self.vector_params)
+        expected_put = np.array([[-4.69071412e-01, -3.04750685e-01, -1.64990710e-01],
+                                 [-3.77896910e-01, -2.36248923e-01, -1.15275505e-01],
+                                 [-2.80139757e-01, -1.65995197e-01, -6.77782897e-02],
+                                 [-1.67672008e-01, -9.12383748e-02, -2.42262934e-02],
+                                 [-2.73380139e-02, -1.34089069e-02, -5.04131783e-05]])
+        np_test.assert_allclose(test_put, expected_put, rtol=1e-5)
+
+    def test_rho_vector_df(self):
+        """Test Theta - pd.DataFrame output case"""
+
+        # request Pandas DataFrame as output format
+        self.vector_params["np_output"] = False
+
+        # call
+        test_call = self.call_opt.rho(**self.vector_params)
+        expected_call = pd.DataFrame(data=[[2.08128741e-01, 3.72449469e-01, 5.12209444e-01],
+                                           [1.39670999e-01, 2.81318986e-01, 4.02292404e-01],
+                                           [7.76651463e-02, 1.91809707e-01, 2.90026614e-01],
+                                           [2.49657984e-02, 1.01399432e-01, 1.68411513e-01],
+                                           [2.17415573e-05, 1.39508485e-02, 2.73093423e-02]],
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_call.rename_axis("S", axis='columns', inplace=True)
+        expected_call.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_call, expected_call, check_less_precise=True)
+
+        # put
+        test_put = self.put_opt.rho(**self.vector_params)
+        expected_put = pd.DataFrame(data=[[-4.69071412e-01, -3.04750685e-01, -1.64990710e-01],
+                                          [-3.77896910e-01, -2.36248923e-01, -1.15275505e-01],
+                                          [-2.80139757e-01, -1.65995197e-01, -6.77782897e-02],
+                                          [-1.67672008e-01, -9.12383748e-02, -2.42262934e-02],
+                                          [-2.73380139e-02, -1.34089069e-02, -5.04131783e-05]],
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_put.rename_axis("S", axis='columns', inplace=True)
+        expected_put.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_put, expected_put, check_less_precise=True)
+
+    def test_Implied_Vol_scalar(self):
+        """Test Implied Volatility - scalar case"""
+
+        # call
+        test_call = scalarize(self.call_opt.implied_volatility(**self.scalar_params))
+        expected_call = 0.2
+        self.assertAlmostEqual(test_call, expected_call)
+
+        # put
+        test_put = scalarize(self.put_opt.implied_volatility(**self.scalar_params))
+        expected_put = 0.2
+        self.assertAlmostEqual(test_put, expected_put)
+
+    def test_Implied_Vol_vector_np(self):
+        """Test Implied Volatility - np.ndarray output case"""
+
+        # call
+        test_call = self.call_opt.implied_volatility(**self.vector_params)
+        expected_call = 0.2 + np.zeros_like(test_call)
+        np_test.assert_allclose(test_call, expected_call, rtol=1e-5)
+
+        # put
+        test_put = self.put_opt.implied_volatility(**self.vector_params)
+        expected_put = 0.2 + np.zeros_like(test_put)
+        np_test.assert_allclose(test_put, expected_put, rtol=1e-5)
+
+    def test_Implied_Vol_vector_df(self):
+        """Test Implied Volatility - pd.DataFrame output case"""
+
+        # request Pandas DataFrame as output format
+        self.vector_params["np_output"] = False
+
+        # call
+        test_call = self.call_opt.implied_volatility(**self.vector_params)
+        expected_call = pd.DataFrame(data=0.2 + np.zeros_like(test_call),
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_call.rename_axis("S", axis='columns', inplace=True)
+        expected_call.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_call, expected_call, check_less_precise=True)
+
+        # put
+        test_put = self.put_opt.implied_volatility(**self.vector_params)
+        expected_put = pd.DataFrame(data=0.2 + np.zeros_like(test_put),
+                                     index=self.vector_params["t"],
+                                     columns=self.vector_params["S"])
+        expected_put.rename_axis("S", axis='columns', inplace=True)
+        expected_put.rename_axis("t", axis='rows', inplace=True)
+        pd_test.assert_frame_equal(test_put, expected_put, check_less_precise=True)
 
 
 if __name__ == '__main__':
